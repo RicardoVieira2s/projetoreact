43b7d231c3cdd0a2cec3b7ab5fc3a41a561695a9
Ricardo Vieira <vieiraricardodias@gmail.com>
2021-12-28 22:15:33 +0000

    upd

--- src/components/topBar/topBar.js
+++ src/components/topBar/topBar.js
@@ -25,14 +25,15 @@ import PersonIcon from '@mui/icons-material/Person';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 
 const Search = styled('div')(({ theme }) => ({
+    height: '40px',
     position: 'relative',
     borderRadius: '20px',
     backgroundColor: alpha(theme.palette.common.white, 0.15),
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(1),
     marginLeft: 0,
     width: '100%',
     [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
+        marginLeft: theme.spacing(2),
         width: '50%',
     },
 }));
@@ -49,6 +50,7 @@ const SearchIconWrapper = styled('div')(({ theme }) => ({
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
     color: "#415A77",
+    height: '40px',
     '& .MuiInputBase-input': {
         padding: theme.spacing(1, 1, 1, 0),
         // vertical padding + font size from searchIcon
@@ -56,7 +58,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
         transition: theme.transitions.create('width'),
         width: '100%',
         [theme.breakpoints.up('md')]: {
-            width: '95ch',
+            width: '20ch',
         },
     },
 }));
@@ -194,13 +196,13 @@ export default function PrimarySearchAppBar(props) {
     );
 
     return (
-        <Box sx={{ flexGrow: 1, height: 90, }}>
+        <Box sx={{ flexGrow: 1}}>
             <AppBar
                 position="static"
                 style={{
                     background: '#0D1B2A',
                     color: '#C4C4C4',
-                    height: "100%",
+                    height: "75px",
                 }}
             >
                 <Toolbar
@@ -227,19 +229,20 @@ export default function PrimarySearchAppBar(props) {
                         style={{
                             background: '#C4C4C4',
                             color: '#0D1B2A',
+                            
                         }}
                     >
                         <SearchIconWrapper>
-                            <SearchIcon
-                                sx={{
-                                    bgcolor: '#415A77', color: '#000',
-                                    borderRadius: '20px',
-                                }}
+                            <SearchIcon 
+                            style={{
+                                marginLeft: '-5px',
+                            }}
                             />
                         </SearchIconWrapper>
                         <StyledInputBase
                             placeholder="Pesquisar jogo..."
                             inputProps={{ 'aria-label': 'search' }}
+                            style={{marginLeft: '-5px',}}
                         />
                     </Search>
                     <Box sx={{ flexGrow: 1 }} />
@@ -281,11 +284,11 @@ export default function PrimarySearchAppBar(props) {
                                 sx={{
                                     bgcolor: '#C4C4C4', color: '#415A77',
                                     ':hover': {
-                                        bgcolor: '#415A77',
-                                        color: '#C4C4C4'
+                                        bgcolor: '#C4C4C4',
+                                        color: '#415A77',
                                     },
                                     borderRadius: '20px',
-
+                                    height: '40px'
                                 }}
                                 alignItems='center'
                                 aria-controls={menuId}
@@ -296,7 +299,7 @@ export default function PrimarySearchAppBar(props) {
                                 onClick={handleProfileMenuOpen}
                                 endIcon={<KeyboardArrowDownIcon />}
                             >
-                                <AccountCircleIcon style={{ marginRight: "15px" }} />
+                                <AccountCircleIcon style={{ marginRight: "10px", marginLeft: '-10px'}} />
                                 {props.userAccount}
                             </Button>
                         </IconButton>

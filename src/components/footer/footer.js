fbf4951ac1f10652142e1755e4efebe33ce52ffe
Diogo Jesus <diogojesus92@ua.pt>
2022-01-05 22:32:58 +0000

    footer final

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,68 +3,94 @@ import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
 import Divider from '@material-ui/core/Divider'
 import Typography from '@material-ui/core/Typography'
-import Grid from '@material-ui/core/Grid'
-import {
-    makeStyles,
-    ThemeProvider,
-    createTheme,
-} from '@material-ui/core/styles'
+import { Link } from 'react-router-dom'
+import { COLOR_RICH_BLACK, COLOR_OXFORD_BLUE, COLOR_BDAZZLED_BLUE, COLOR_SHADOW_BLUE, COLOR_PLATINIUM } from '../utils/color'
 
-import { ColumnToRow, Item } from '@mui-treasury/components/flex'
-import { NavMenu, NavItem } from '@mui-treasury/components/menu/navigation'
-import {
-    EmailSubscribe,
-    EmailTextInput,
-    SubmitButton,
-} from '@mui-treasury/components/EmailSubscribe'
-import {
-    CategoryProvider,
-    CategoryTitle,
-    CategoryItem,
-} from '@mui-treasury/components/menu/category'
-import {
-    SocialProvider,
-    SocialLink,
-} from '@mui-treasury/components/socialLink'
 
-import { useMagCategoryMenuStyles } from '@mui-treasury/styles/categoryMenu/mag'
-import { usePoofSocialLinkStyles } from '@mui-treasury/styles/socialLink/poof'
+import IconButton from '@mui/material/IconButton'
+
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import InstagramIcon from '@mui/icons-material/Instagram'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+import { ColumnToRow, Item, Row } from '@mui-treasury/components/flex'
+import { EmailSubscribe, EmailTextInput, SubmitButton } from '@mui-treasury/components/EmailSubscribe'
+
 import { useReadyEmailSubscribeStyles } from '@mui-treasury/styles/emailSubscribe/ready'
-import { usePlainNavigationMenuStyles } from '@mui-treasury/styles/navigationMenu/plain'
 
-const darkTheme = createTheme({ palette: { type: 'dark' } })
 
 const useStyles = makeStyles(({ palette, typography }) => ({
+
     footer: {
-        backgroundColor: '#0D1B2A',
+        backgroundColor: COLOR_RICH_BLACK,
         width: '100%',
         position: 'fixed',
         bottom: '0',
     },
     newsletterText: {
         color: '#fff',
-        fontSize: '0.875rem',
+        fontFamily: 'Viga',
+        fontSize: '20px',
     },
     form: {
-        margin: 0,
-        minWidth: 343,
-        fontSize: '0.875rem',
+        display: 'flex',
+        position: 'relative',
+        width: '100%',
     },
-    legalLink: {
-        textTransform: 'uppercase',
-        fontWeight: 'bold',
-        fontSize: '0.75rem',
-        justifyContent: 'center',
-        color: palette.text.hint,
-        letterSpacing: '0.5px',
+    input: {
+        minWidth: '100px',
+        width: '350px',
+        fontFamily: 'Viga',
+        fontSize: '20px',
+        flex: 1,
+        padding: '0.5rem 1rem',
+        paddingRight: '7.7rem',
+        backgroundColor: '#C4C4C4',
+        color: '#E0E1DDF',
+        border: 'none',
+        borderRadius: 5,
+        outline: 'none',
+        transition: 'box-shadow 0.3s ease-out',
+    },
+    submit: {
+        fontFamily: 'Viga',
+        fontSize: '20px',
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        borderRadius: 5,
+        backgroundColor: '#415A77',
+        color: '#E0E1DD',
+        padding: '0.5rem 1rem',
+        transition: 'transform 0.2s',
+        transformOrigin: 'right',
+        '&:hover, &:focus': {
+            backgroundColor: '#1b263b',
+            color: '#E0E1DD'
+        },
+        '&:active': {
+            transform: 'scale(0.92)'
+        }
     },
     divider: {
         height: 2,
         margin: '-1px 0',
-        backgroundColor: '#778DA9',
+        backgroundColor: COLOR_SHADOW_BLUE,
     },
     text: {
-        color: '#778DA9'
+        color: COLOR_SHADOW_BLUE,
+        fontSize: '20px'
+    },
+    textNewsletter: {
+        color: COLOR_SHADOW_BLUE,
+        fontSize: '20px'
+    },
+    textCopy: {
+        color: COLOR_SHADOW_BLUE,
+        fontSize: '15px'
     },
     overlay: {
         position: 'absolute',
@@ -80,7 +106,8 @@ const useStyles = makeStyles(({ palette, typography }) => ({
         },
     },
     icon: {
-        color: '#778DA9',
+        color: COLOR_BDAZZLED_BLUE,
+        backgroundColor: 'transparent',
     },
     info: {
         ...typography.caption,
@@ -92,66 +119,85 @@ const useStyles = makeStyles(({ palette, typography }) => ({
 export const Footer = React.memo(function ArcAppFooter() {
     const classes = useStyles()
     return (
-        <Box className={classes.footer}>
-            <Box px={2} py={10}>
-                <Container disableGutters>
-                    <Grid container spacing={4}>
-                        <Grid item xs={12} md={4} lg={3}>
-                            <SocialProvider useStyles={usePoofSocialLinkStyles}>
-                                <SocialLink brand={'Twitter'} className={classes.icon} />
-                                <SocialLink brand={'Instagram'} className={classes.icon} />
-                                <SocialLink brand={'FacebookCircle'} className={classes.icon} />
-                            </SocialProvider>
-                        </Grid>
-                        <Grid item xs={12} md={8} lg={3} style={{ marginLeft: 'auto' }}>
-                            <Typography className={classes.text} >
+        <Box className={classes.footer} >
+            <Container >
+                <Box pt={8} pb={2} className={classes.top} position={'relative'}>
+                    <Row wrap alignItems="center">
+                        <Item grow={2}>
+                            <Item >
+                                <FacebookRoundedIcon sx={{
+                                    color: COLOR_BDAZZLED_BLUE,
+                                    ':hover': {
+                                        color: COLOR_PLATINIUM,
+                                    },
+                                    mt: '6px',
+                                }}
+                                    fontSize="large" />
+                                <TwitterIcon sx={{
+                                    color: COLOR_BDAZZLED_BLUE,
+                                    ':hover': {
+                                        color: COLOR_PLATINIUM,
+                                    },
+                                    mt: '6px',
+                                }}
+                                    fontSize="large" />
+                                <InstagramIcon sx={{
+                                    color: COLOR_BDAZZLED_BLUE,
+                                    ':hover': {
+                                        color: COLOR_PLATINIUM,
+                                    },
+                                    mt: '6px',
+                                }}
+                                    fontSize="large" />
+                            </Item>
+                        </Item>
+                        <Item paddingRight= '15px'>
+                            <Typography className={classes.textNewsletter}>
                                 Newsletter
                             </Typography>
-                            <Item>
-                                <EmailSubscribe
-                                    className={classes.form}
-                                    onSubmit={email => alert(`Your email is ${email}.`)}
-                                    useStyles={useReadyEmailSubscribeStyles}
-                                    inputClearedAfterSubmit
-                                >
-                                    <EmailTextInput placeholder="Email..." />
-
-                                    {/* FIXME: ARRANJAR O BOTÃO DE SUBMIT */}
-
-                                    {/* <SubmitButton>Subscrever</SubmitButton> */}
-
-                                </EmailSubscribe>
-                            </Item>
-
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Box>
-            <Container disableGutters>
-                <Divider className={classes.divider} />
-            </Container>
-            <Box px={1} py={1} className={classes.bottom}>
-                <Container disableGutters>
-                    <ColumnToRow
-                        at={'md'}
-                        columnStyle={{ alignItems: 'center' }}
-                        rowStyle={{ alignItems: 'unset' }}
-                        justifyContent="center"
-                    >
-                        <Item>
-                            <Box>
-                                <Typography
-                                    component={'p'}
-                                    variant={'caption'}
-                                    className={classes.text}
-                                >
-                                    © 2022 GOOFR. All rights reserved.
-                                </Typography>
-                            </Box>
                         </Item>
-                    </ColumnToRow>
-                </Container>
-            </Box>
-        </Box>
+                        <Item>
+                            <EmailSubscribe
+                                className={classes.form}
+                                onSubmit={email => alert(`Your email is ${email}.`)}
+                                useStyles={useStyles}
+                                inputClearedAfterSubmit
+                            >
+                                <EmailTextInput placeholder="Email..." />
+                                <SubmitButton>Subscrever</SubmitButton>
+                            </EmailSubscribe>
+                        </Item>
+                    </Row>
+                </Box>
+                <Divider className={classes.divider} />
+                <Box pt={3} pb={1}>
+                    <Container>
+                        <Row wrap
+                            alignItems="center"
+                            justifyContent="center"
+                        >
+                            <Item>
+                                <Box>
+                                    <Link to="/">
+                                        <img src="/images/store.png" alt="" />
+                                    </Link>
+                                </Box>
+                            </Item>
+                            <Item>
+                                <Box>
+                                    <Typography
+                                        component={'p'}
+                                        variant={'caption'}
+                                        className={classes.textCopy}
+                                    >
+                                        © 2022 GOOFR. All rights reserved.
+                                    </Typography>
+                                </Box>
+                            </Item>
+                        </Row>
+                    </Container>
+                </Box>
+            </Container>
+        </Box >
     )
 })

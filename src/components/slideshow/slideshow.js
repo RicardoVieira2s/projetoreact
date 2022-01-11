53e393af4cb7d753153e657dc50b03ac51295e0e
Diogo Jesus <diogojesus92@ua.pt>
2022-01-11 01:44:16 +0000

    d

--- src/components/slideshow/slideshow.js
+++ src/components/slideshow/slideshow.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import CoverFlow from 'coverflow-react';
 import { makeStyles } from "@material-ui/core";
-import CoverFlow from 'coverflow-react';
 
 const imagesArr = [
     'https://s2.glbimg.com/dLHiEFov94ONVIVLP2V85FWh7hE=/0x0:695x390/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2019/3/t/9vl0fdRyi9SvlPOe33yA/subway-surfers-mais-baixado-downloads-decada-2010-2019.jpg',
